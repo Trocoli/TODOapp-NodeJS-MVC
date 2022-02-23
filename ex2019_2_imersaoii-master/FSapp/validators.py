7d0827073054ce2192b67c80437d649aa82ad53a
Trocoli <46879453+Trocoli@users.noreply.github.com>
2022-02-23 10:36:46 -0300

    Delete ex2019_2_imersaoii-master directory

--- ex2019_2_imersaoii-master/FSapp/validators.py
+++
@@ -1,8 +0,0 @@
-from django.core.exceptions import ValidationError
-
-def Validar_Matricula(value):
-    if value.isdigit()== True:
-        return value
-    else:
-        raise ValidationError('A matrícula só possui números!')
-
